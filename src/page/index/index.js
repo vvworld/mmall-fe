145064edae08f3aa7beeffbf2d43cfd454b9c3cc
huangxiaowei <921169792@qq.com>
2018-07-20 02:07:12 +0800

    common

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,3 +1,21 @@
-require("../module.js");
-require("../common/index.js");
-require('./index.css');
+console.log('123');
+require('page/common/nav/index.js');
+var navSide = require('page/common/nav-side/index.js')
+require('page/common/header/index.js');
+var _mm = require('util/mm.js');
+navSide.init({name:'user-center'});
+// _mm.request({
+// 	url: '/product/list.do?keyword=1o',
+// 	success: function(res){
+// 		console.log(res);
+// 	},
+// 	error: function(err){
+// 		console.log(err);
+// 	},
+// })
+// console.log(_mm.getUrlParam('test'));
+// var html = "<div>{{data}}<div>";
+// var data = {
+// 	data : 123
+// }
+// console.log(_mm.renderHtml(html,data));
--- webpack.config.js
+++ webpack.config.js
@@ -9,25 +9,37 @@ var WEBPACK_ENV       = process.env.WEBPACK_ENV || 'dev';
 console.log(WEBPACK_ENV);
 
 //获取html-webpack-plugin参数的方法
-var getHtmlConfig     = function(name){
+var getHtmlConfig     = function(name,title){
   return {
     template: './src/view/'+name+'.html',
     filename: 'view/'+name+'.html',
+    title   : title,
     inject  : true,
     hash    : true,
-    chunks  : ['commons',name]
+    chunks  : ['common',name]
   }
 }
 var config            = {
   entry: {
-  	// 'common' : ['./src/page/common/index.js'],
-  	'index' : ['./src/page/index/index.js'],
-  	'login' : ['./src/page/login/index.js'],
+  	'common'      : ['./src/page/common/index.js'],
+  	'index'       : ['./src/page/index/index.js'],
+  	'login'       : ['./src/page/login/index.js'],
+    'result'      : ['./src/page/result/index.js']
   },
   output: {
     filename: 'js/[name].js',
     publicPath: '/dist',//没有这个，dev-server界面不会跟着一起发生变化
     path: path.resolve(__dirname, './dist')
+  },
+  resolve: {
+    //设置模块别名，便于我们更方便引用
+    alias: {
+      node_modules    : __dirname + '/node_modules',
+      util            : __dirname + '/src/util',
+      page            : __dirname + '/src/page',
+      service         : __dirname + '/src/service',
+      image           : __dirname + '/src/image',
+    }
   },
   module: {
     rules: [
@@ -44,6 +56,10 @@ var config            = {
       { 
         test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, 
         loader: 'url-loader?limit=100&name=/resource/[name].[ext]' 
+      },
+      {
+        test: /\.string$/,
+        loader:'html-loader'
       }
     ]
   },
@@ -51,8 +67,9 @@ var config            = {
     // 把CSS单独打包到文件里
     new Ex('css/[name].css'),
     // html模板的处理
-    new HtmlWebpackPlugin(getHtmlConfig('index')),
-    new HtmlWebpackPlugin(getHtmlConfig('login'))
+    new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
+    new HtmlWebpackPlugin(getHtmlConfig('login','用户登录')),
+    new HtmlWebpackPlugin(getHtmlConfig('result','操作结果'))
     // 独立通用模块js/base.js,然而webpack4以上被弃用
     // new webpack.optimize.CommonsChunkPlugin({
     //   name : 'commons',
